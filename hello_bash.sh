4fde8d943b784baab6f0cac0d5360130f51fb024
Cora Albers <cralbers3@gmail.com>
2026-06-23 11:58:02 -0400

    initial commit

---
+++ hello_bash.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Hello world in bash
+# This is a jupyter notebook using bash magic
+
+# In[1]:
+
+
+get_ipython().run_cell_magic('bash', '', 'echo "Hello world from bash!"\n')
+
